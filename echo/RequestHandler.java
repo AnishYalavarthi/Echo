474e738f90561c191c82c566b6cc82e8a15a8e87
AnishYalavarthi <anish.yalavarthi@gmail.com>
2025-04-28 17:41:15 -0700

    Modified SecurityHandler to function properly

--- echo/RequestHandler.java
+++ echo/RequestHandler.java
@@ -3,7 +3,7 @@ package echo;
 import java.net.Socket;
 
 public class RequestHandler extends Correspondent implements Runnable {
-    protected boolean active; // response can set to false to terminate thread
+    protected boolean active;
     public RequestHandler(Socket s) {
         super(s);
         active = true;
@@ -12,11 +12,11 @@ public class RequestHandler extends Correspondent implements Runnable {
         super();
         active = true;
     }
-    // override in a subclass:
+
     protected String response(String request) throws Exception {
         return "echo: " + request;
     }
-    // any housekeeping can be done by an override of this:
+
     protected void shutDown() {
         if (Server.DEBUG) {
             System.out.println("handler shutting down");
@@ -26,22 +26,18 @@ public class RequestHandler extends Correspondent implements Runnable {
     public void run() {
         while(active) {
             try {
-                // receive request
                 String request = receive();
                 if(request.equals("quit")) {
                     shutDown();
                     break;
                 }
-                // send response
                 send(response(request));
-                // sleep
                 Thread.sleep(50);
             } catch(Exception e) {
                 send(e.getMessage() + "... ending session");
                 break;
             }
         }
-        // close
         close();
     }
 }
